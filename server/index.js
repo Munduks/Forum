9c18df9621919266feddfad160c185a348477cac
Raimonda <raimondastonkute@gmail.com>
2023-07-07 09:52:16 +0300

    add server routes & delate client & install new 1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
-/* eslint-disable consistent-return */
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -9,44 +8,11 @@ const port = process.env.PORT || 8080;
 const URI = process.env.DB_CONNECTION_STRING;
 const dbName = process.env.DB_NAME;
 
-// console.log(port, URI, dbName);
-
 const app = express();
 app.use(cors());
-app.use(express.json()); // aplikacija moka apdoroti JSON formatu ateinancius requestu
+app.use(express.json());
 
 const client = new MongoClient(URI);
-
-app.post('/register', async (req, res) => {
-  try {
-    const con = await client.connect();
-    const { email } = req.body;
-
-    // Patikriname, ar vartotojas jau yra duomenų bazėje
-    const existingUser = await con
-      .db(dbName)
-      .collection('users')
-      .findOne({ email });
-
-    if (existingUser) {
-      // Jei vartotojas jau yra, grąžiname klaidos pranešimą
-      res.status(400).json({ message: 'Toks vartotojas jau yra.' });
-      return;
-    }
-
-    // Jei vartotojo nėra, registruojame naują vartotoją
-    const user = {
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    };
-    const data = await con.db(dbName).collection('users').insertOne(user);
-    res.status(200).json(data);
-    await con.close();
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
 
 app.get('/register', async (req, res) => {
   try {
@@ -59,21 +25,46 @@ app.get('/register', async (req, res) => {
   }
 });
 
+app.post('/register', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const { name, email, password } = req.body;
+    const user = {
+      name,
+      email,
+      password,
+    };
+
+    const existingUser = await con
+      .db(dbName)
+      .collection('users')
+      .findOne({ email });
+
+    if (existingUser) {
+      res.status(400).json({ error: 'Toks vartotojas jau yra.' });
+      return;
+    }
+
+    const data = await con.db(dbName).collection('users').insertOne(user);
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 app.post('/login', async (req, res) => {
   try {
     const con = await client.connect();
     const { email, password } = req.body;
 
-    // Patikriname, ar vartotojas su nurodytu el. paštu yra duomenų bazėje
     const user = await con.db(dbName).collection('users').findOne({ email });
 
     if (!user || user.password !== password) {
-      // Jei vartotojas nerastas arba nesutampa slaptažodis, grąžiname klaidos pranešimą
       res.status(401).json({ message: 'Klaidingai suvesti duomenys.' });
       return;
     }
 
-    // Jei vartotojas rastas ir slaptaž teisingas, perduodame informac apie prisijungusį vartotoją
     res.status(200).json({ message: 'Prisijungta sėkmingai.', user });
 
     await con.close();
@@ -82,49 +73,233 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// app.get('/ownersWithPets', async (req, res) => {
-//   try {
-//     const con = await client.connect();
-//     const data = await con
-//       .db(dbName)
-//       .collection('owners')
-//       .aggregate([
-//         {
-//           $lookup: {
-//             from: 'pets',
-//             localField: '_id',
-//             foreignField: 'ownerId',
-//             as: 'pets',
-//           },
-//         },
-//       ])
-//       .toArray();
-//     await con.close();
-//     res.send(data);
-//   } catch (error) {
-//     res.status(500).send(error);
-//   }
-// });
-// // query params
-// app.get('/owners', async (req, res) => {
-//   try {
-//     const { sort } = req.query;
-//     const sortType = sort === 'asc' ? 1 : -1;
+app.get('/users', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('users').find().toArray();
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
+app.get('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const con = await client.connect();
+    const data = await con
+      .db(dbName)
+      .collection('users')
+      .findOne(new ObjectId(id));
+    await con.close();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get('/questions', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const data = await con.db(dbName).collection('questions').find().toArray();
+    await con.close();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// app.post('/questions', async (req, res) => {
+//   try {
 //     const con = await client.connect();
+//     const currentDate = new Date();
+//     const question = {
+//       questionText: req.body.questionText,
+//       questionDate: currentDate,
+//       updated: false,
+//       answers: [],
+//     };
 //     const data = await con
 //       .db(dbName)
-//       .collection('owners')
-//       .find()
-//       .sort({ income: sortType }) // 1 dideja -1 mazeja
-//       .toArray();
+//       .collection('questions')
+//       .insertOne(question);
+//     res.status(200).json(data);
 //     await con.close();
-//     res.send(data);
 //   } catch (error) {
 //     res.status(500).send(error);
 //   }
 // });
+
+app.post('/questions', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const currentDate = new Date();
+    const question = {
+      questionText: req.body.questionText,
+      questionDate: currentDate,
+      updated: false,
+    };
+    const data = await con
+      .db(dbName)
+      .collection('questions')
+      .insertOne(question);
+    res.status(200).json(data);
+    await con.close();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+app.put('/questions/:id', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const { id } = req.params;
+
+    const data = await con
+      .db(dbName)
+      .collection('questions')
+      .updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { questionText: req.body.questionText, updated: true } },
+      );
+    await con.close();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.delete('/questions/:id', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const { id } = req.params;
+    const data = await con
+      .db(dbName)
+      .collection('questions')
+      .deleteOne({ _id: new ObjectId(id) });
+    await con.close();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// app.get('/questions/:id/answers', async (req, res) => {
+//   try {
+//     const con = await client.connect();
+//     const { id } = req.params;
+//     const data = await con
+//       .db(dbName)
+//       .collection('answers')
+//       .find({
+//         questionId: new ObjectId(id),
+//       })
+//       .toArray();
+//     await con.close();
+//     res.status(200).json(data);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
+
+app.get('/questions/:id/answers', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const { id } = req.params;
+    const data = await con
+      .db(dbName)
+      .collection('questions')
+      .findOne({ _id: new ObjectId(id) });
+
+    if (!data) {
+      res.status(404).json({ message: 'Question not found' });
+      return;
+    }
+
+    const answers = data.answers;
+    await con.close();
+    res.status(200).json(answers);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.put('/answers/:id', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const { id } = req.params;
+    const { answer } = req.body;
+    const data = await con
+      .db(dbName)
+      .collection('answers')
+      .updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { answer: answer, updated: true } },
+      );
+    await con.close();
+
+    if (data.matchedCount === 0) {
+      res.status(404).json({ message: 'Answer not found' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Answer updated successfully' });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.delete('/answers/:id', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const { id } = req.params;
+    const data = await con
+      .db(dbName)
+      .collection('answers')
+      .deleteOne({ _id: new ObjectId(id) });
+    await con.close();
+    res.status(200).json({ message: 'successfully deleted...' });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// app.post('/questions/:id/answers', async (req, res) => {
+//   try {
+//     const con = await client.connect();
+//     const { id } = req.params;
+//     const data = await con
+//       .db(dbName)
+//       .collection('questions')
+//       .updateOne({ _id: new ObjectId(id) }, { $push: { answers: req.body } });
+//     await con.close();
+//     res.status(200).json(data);
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// });
+
+app.post('/questions/:id/answers', async (req, res) => {
+  try {
+    const con = await client.connect();
+    const { id } = req.params;
+    const answer = await con
+      .db(dbName)
+      .collection('answers')
+      .insertOne({
+        answer: req.body.answer,
+        count: 0,
+        updated: false,
+        created: new Date(),
+        questionId: new ObjectId(id),
+      });
+    res.status(200).json(answer);
+    await con.close();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`server is running on the ${port} port`);
+  console.log(`Server is running on port ${port}`);
 });
